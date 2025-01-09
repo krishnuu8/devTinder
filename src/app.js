922f777cbf8f17406844e2144bb122239ff4098d
Krish <krishnu821@gmail.com>
2025-01-09 22:45:41 +0530

    testing get,post and delete

--- src/app.js
+++ src/app.js
@@ -1,18 +1,41 @@
 const express=require("express");
 const app=express(); //creates a express app instance;uses functional approach
+
+app.get("/user",(req,res)=>{
+    res.send({
+        FirstName:"Krish",
+        LastName:"Arora",
+        Age:21
+    })
+})
+app.post("/user",(req,res)=>{
+    //saving data to DB
+    res.send("USER SAVED SUCCESSFULLY TO THE DB!")
+})
+app.delete("/user",(req,res)=>{
+    //deleting user from DB
+    res.send("USER DELETED SUCCESSFULLY FROM THE DB!")
+})
+app.use("/hello/1",(req,res)=>{
+    res.send("Hello from one!") 
+    
+})//request handler
 app.use("/hello",(req,res)=>{
     res.send("HELLO HELLO HELLO!!!")
+})
+app.use("/test/1",(req,res)=>{
+    res.send("Testing from one!") 
+    
 })
 app.use("/test",(req,res)=>{
     res.send("MIKE TESTING,MIKE TESTING!")
 })
-app.use("",(req,res)=>{
-    res.send("BHAISAAB!!!!")
-})//request handler
+app.use("/",(req,res)=>{
+    res.send("BHAISAAB!!!!") 
+    
+})
 app.listen(7777,()=>{
     console.log("SERVER IS CREATED SUCCESSFULLY!");
 });
 
-//installing nodemon so that my server is automatically refreshed
-//installing it as global one on the system!
-
+//jo pehle match krgya usko execute krdo!

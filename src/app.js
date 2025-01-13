b142cf855f75a19c8df44b1507789ea24cad0f5e
Krish <krishnu821@gmail.com>
2025-01-13 17:44:20 +0530

    delete-api

--- src/app.js
+++ src/app.js
@@ -4,10 +4,32 @@ const{connectDb}=require("./config/database")
 const {User}=require("./models/user")
 //now we need to convert JSON=>js object so that server can read it and we can manupilate it
 app.use(express.json())
+//get user by email
+app.get("/user",async (req,res)=>{
+    const userEmail=req.body.emailId
+   const userData = await User.find({emailId:userEmail})
+   if(userData.length===0){
+    res.status(401).send("Something went wrong!")
+   }
+   else{
+    res.send(userData)
+   }
+})
+// get/feed api to get all the users from the database
+app.get("/feed",async (req,res)=>{
+   try{
+    const users = await User.find({})
+   if(!users){
+    res.status(401).send("Something went wrong!")
+   }else{
+    res.send(users)
+   }
+   }catch(err){
+res.status(400).send("Something went Wrong")
+}
+})
+//create a user
 app.post("/signup",async (req,res)=>{
-
-     
-
     // creating instance(object) of User Model
     const newUser1=new User(req.body)
    try {
@@ -17,9 +39,20 @@ app.post("/signup",async (req,res)=>{
     res.status(400).send("Error creating in user,Error:",err)
    }
 })
+//delete a user
+app.delete("/user",async (req,res)=>{
+    try{
+    const userId=req.body.userId
+    await User.findByIdAndDelete(userId)
+    res.send("User deleted successfully!")
+    }catch(err){
+        res.status(400).send("Error in deleting your account,Error:",err)
+    }
+})
 
 connectDb()
 .then(()=>{
+
     console.log("Database connected successfully!")
     app.listen(7777,()=>{
         console.log("SERVER IS CREATED SUCCESSFULLY!");

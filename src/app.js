887844628d5f945cebeb3c25e03a247034566822
Krish <krishnu821@gmail.com>
2025-01-14 21:43:10 +0530

    data-validation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express=require("express");
 const app=express(); 
+const mongoose=require("mongoose")
 const{connectDb}=require("./config/database") 
 const {User}=require("./models/user")
 //now we need to convert JSON=>js object so that server can read it and we can manupilate it
@@ -36,8 +37,9 @@ app.post("/signup",async (req,res)=>{
     await newUser1.save();
     res.send("User created Successfully!")
    } catch (err) {
-    res.status(400).send("Error creating in user,Error:",err)
-   }
+    
+    res.status(400).send("Error creating in user,Error:"+err)
+}
 })
 //delete a user
 app.delete("/user",async (req,res)=>{
@@ -46,22 +48,38 @@ app.delete("/user",async (req,res)=>{
     await User.findByIdAndDelete(userId)
     res.send("User deleted successfully!")
     }catch(err){
-        res.status(400).send("Error in deleting your account,Error:",err)
+        res.status(400).send("Error in deleting your account,Error:"+err)
     }
 })
 //update user
-app.patch("/user",async (req,res)=>{
+app.patch("/user/:userId",async (req,res)=>{
+
     try {
-        const userId=req.body.userId
-        const data=req.body
+        // const userId=req.body.userId
+        const userId=req.params?.userId//?. makes sure that your code does not break
+        // if (!mongoose.Types.ObjectId.isValid(userId)) {
+        //     throw new Error("Invalid userId format");
+        // }
+        // const objectId = new mongoose.Types.ObjectId(userId);
+        const data=req.body //if i include userId in my body ,so then i will need ti include it in update allowed array which i dont want to
+        const UPDATES_ALLOWED=["password","gender","photoURrl","about","skills"] //we dont want to allow update in userId
+        const isAllowed=Object.keys(data).every((k)=>UPDATES_ALLOWED.includes(k)) //matching two arrays:UPDATES_ALLOWED AND KEYS ARRAY 
+        if(!isAllowed){
+        throw new Error("Update not allowed!")
+        }
+        if(data?.skills.length>10){
+            throw new Error("Skills cannot be more than 10")
+        }
         const user=await User.findByIdAndUpdate(userId,data) 
+        console.log(userId)
         console.log(user)       //default is before
         res.send("Data updated Sucessfully")
     } catch (err) {
-        res.status(400).send("Error in updating your details,Error:",err)
+        res.status(400).send("UPDATE Failed:"+err)
     }
 
 })
+//get by user by email
 app.patch("/userbyemail",async (req,res)=>{
     try {
         const email=req.body.emailId
@@ -70,7 +88,7 @@ app.patch("/userbyemail",async (req,res)=>{
         console.log(user)       //default is before
         res.send("Data updated Sucessfully")
     } catch (err) {
-        res.status(400).send("Error in updating your details,Error:",err)
+        res.status(400).send("Error in updating your details,Error:"+err)
     }
 
 })
@@ -88,7 +106,8 @@ connectDb()
 })
 .catch((err)=>console.log("Database not connected,error:",err))
 
-
+let obj={a:1,b:2,c:3}
+console.log(Object.keys(obj))
 
 
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,12 +1,49 @@
 const mongoose=require("mongoose")
-const userSchema=mongoose.Schema({
-    firstName:String,
-    lastName:String,
-    emailId:String,
-    password:String,
-    age:Number,
-    gender:String
-})
+const userSchema=new mongoose.Schema({
+    firstName:{
+        type: String,
+        required:true,
+        minlength:4,
+        maxlength:50
+    },
+    lastName:{
+        type:String
+    },
+    emailId:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        match:[/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/]
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    age:{
+        type:Number,
+        min:18
+    },
+    gender:{
+        type:String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender data is not valid!")
+            }
+        }
+    },
+    photoUrl:{
+        type:String,
+        default:"https://www.istockphoto.com/vector/user-profile-icon-in-flat-style-member-avatar-vector-illustration-on-isolated-gm1460685694-494809101"
+    },
+    about:{
+        type:String,
+        default:"This is a default description!"
+    },
+    skills:{
+    type:[String]
+    }
+},{timestamps:true})
 const User=mongoose.model("User",userSchema)
 module.exports={User}
 
